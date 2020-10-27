d2b4c4e29f5cae0b01734454c6e18ba1a070085b
Andra <andra.cardas@altom.com>
2020-10-27 12:07:50 +0200

    added mac build and readme

---
+++ start_tests_Mac.sh
@@ -0,0 +1,5 @@
+start "" "App\TrashCatMac\TrashCat.app"
+
+python3 TestsPython\\tests\\main_menu_page_test.py
+python3 TestsPython\\tests\\game_play_test.py
+python3 TestsPython\\tests\\start_page_test.py
